c404a384a36b32f54ec95b73c5827ab376af0756
Stefan Ninkovic <stefannninkovic@gmail.com>
2021-03-09 23:33:42 +0100

    CHANGE: Initial Commit

---
+++ src/DingDong.Backend.Web.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DingDong.Backend.Web.Api.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult TestConnection()
+        {
+            return Ok(new { message = "Ich esse gerne Pastelfarben!" });
+        }
+    }
+}
